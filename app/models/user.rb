b2123975f762b95b5c461fb0a8c075ba23d2aaf5
OGNeutron <scottberry91@gmail.com>
2016-07-31 14:29:23 +0100

    Minor change in user model

--- app/models/user.rb
+++ app/models/user.rb
@@ -18,7 +18,7 @@ class User < ApplicationRecord
 
 	def remember 
 		self.remember_token = User.new_token
-		update_attribute(:remember_digest, user.digest(remember_token))
+		update_attribute(:remember_digest, User.digest(remember_token))
 	end
 
 	def forget 
