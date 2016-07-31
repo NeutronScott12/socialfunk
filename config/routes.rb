60b391d18738336c267a19c0f3b4d365e049f28a
OGNeutron <scottberry91@gmail.com>
2016-07-31 15:21:38 +0100

    Fixed Password Reset Urls

--- config/routes.rb
+++ config/routes.rb
@@ -1,9 +1,5 @@
 Rails.application.routes.draw do
 
-  get 'password_resets/new'
-
-  get 'password_resets/edit'
-
 	root 'static_pages#home'
 
 	get '/sign_up' => 'users#new'
@@ -19,6 +15,7 @@ Rails.application.routes.draw do
 
 	resources :account_activations, only: [:edit]
 	resources :users
+	resources :password_resets, only: [:new, :create, :edit, :update]
 
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 end
