61532c8c57f440148eaf150528a1f5ec43c753c7
OGNeutron <scottberry91@gmail.com>
2016-08-01 13:54:32 +0100

    minor changes

--- config/routes.rb
+++ config/routes.rb
@@ -12,7 +12,7 @@ Rails.application.routes.draw do
 
 	delete 'logout' => 'sessions#destroy'
 
-	get '/:id' => 'users#show'
+	match '/:id' => 'users#show'
 
 	resources :account_activations, only: [:edit]
 	resources :users do 
